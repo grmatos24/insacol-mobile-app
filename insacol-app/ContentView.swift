cf7133f2d1e5f41c0b786675c8245c0278dab231
grmatos24 <matosguille96@gmail.com>
2026-05-16 23:35:36 -0500

    Initial Commit

---
+++ insacol-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  insacol-app
+//
+//  Created by Guillermo Matos on 16/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ insacol-app/insacol_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  insacol_appApp.swift
+//  insacol-app
+//
+//  Created by Guillermo Matos on 16/5/26.
+//
+
+import SwiftUI
+
+@main
+struct insacol_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
